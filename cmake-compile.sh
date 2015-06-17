8bf13f6d834da49b86772e01a330fb9c6107456e
zimoun <zimoun.toutoune@gmail.com>
2015-06-16 23:55:01 -0300

    cmake

--- cmake-compile.sh
+++ cmake-compile.sh
@@ -2,10 +2,11 @@
 
 cd $HOME
 
-wget -q http://www.cmake.org/files/v3.2/cmake-3.2.3.tar.gz
-tar -xz cmake-3.2.3.tar.gz
+wget http://www.cmake.org/files/v3.2/cmake-3.2.3.tar.gz
+tar -xzvf cmake-3.2.3.tar.gz
 cd cmake-3.2.3/
 
 ./bootstrap && make
 
 cd $HOME
+return 0
